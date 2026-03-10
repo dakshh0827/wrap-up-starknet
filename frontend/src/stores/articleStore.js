6e0f0683cee800480ef2acb16282902ca18dba16
Daksh Thakran <daksh.thakran05@gmail.com>
2026-03-11 00:26:16 +0530

    Update articleStore.js

--- frontend/src/stores/articleStore.js
+++ frontend/src/stores/articleStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_BASE = '/api';
+const API_BASE = 'https://wrap-up-starknet-2.onrender.com/api';
 
 export const useArticleStore = create((set, get) => ({
   // 1. STATE
